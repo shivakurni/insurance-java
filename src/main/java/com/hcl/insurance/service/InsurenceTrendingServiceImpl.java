53ca378d0b4e893c8dcbc92101d376fbbc7ee062
= <siva3060@gmail.com>
2019-08-29 17:20:10 +0530

    test cases for services

--- src/main/java/com/hcl/insurance/service/InsurenceTrendingServiceImpl.java
+++ src/main/java/com/hcl/insurance/service/InsurenceTrendingServiceImpl.java
@@ -3,9 +3,7 @@ package com.hcl.insurance.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +13,7 @@ import com.hcl.insurance.dto.TredingResponse;
 import com.hcl.insurance.dto.TrendingAllRespose;
 import com.hcl.insurance.entity.Policy;
 import com.hcl.insurance.entity.PolicyPurchase;
-import com.hcl.insurance.exception.BankProductException;
+import com.hcl.insurance.exception.InsurancePolicyException;
 import com.hcl.insurance.repository.PolicyPurchaseRepository;
 import com.hcl.insurance.repository.PolicyRepository;
 
@@ -43,7 +41,7 @@ public class InsurenceTrendingServiceImpl implements InsurenceTrendingService {
 			Optional<Policy> policy = policyRepository.findById(tredingResponse.getPolicyId());
 
 			if (!policy.isPresent())
-				throw new BankProductException(" policy not present");
+				throw new InsurancePolicyException(" policy not present");
 
 			trendingAllRespose.setPolicyName(policy.get().getPolicyName());
 			trendingAllRespose.setPolicyId(policy.get().getPolicyId());
@@ -59,27 +57,13 @@ public class InsurenceTrendingServiceImpl implements InsurenceTrendingService {
 	@Override
 	public List<TrendingAllRespose> trendingTop() {
 		List<PolicyPurchase> policyPurchaseList = policyPurchaseRepository.trendngsTop();
-		
-		if(policyPurchaseList.isEmpty())
-			throw new BankProductException("");
-		
-//		
-//		Map<PolicyPurchase, Long> result = policyPurchaseList.stream()
-//				.collect(Collectors.groupingBy(PolicyPurchase::policyId,  Collectors.counting()));
-		
-		Map<String, Long> counting = policyPurchaseList.stream().collect(
-                Collectors.groupingBy(PolicyPurchase::getName, Collectors.counting()));
 
-		 for (Entry<String, Long> entry : counting.entrySet())  {
-			 
-			 TredingResponse tredingResponse=new TredingResponse();
-			
-			 
-	            System.out.println("Key = " + entry.getKey() + 
-	                             ", Value = " + entry.getValue()); 
-		 }
-	  
-		
+		if (policyPurchaseList.isEmpty())
+			throw new InsurancePolicyException("");
+
+		Map<String, Long> counting = policyPurchaseList.stream()
+				.collect(Collectors.groupingBy(PolicyPurchase::getName, Collectors.counting()));
+
 		for (PolicyPurchase policyPurchase : policyPurchaseList) {
 			policyPurchase.getPolicyId();
 
