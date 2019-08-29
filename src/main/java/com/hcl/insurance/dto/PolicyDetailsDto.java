276c1aaf1e8414d34614219e925983befa7c1149
User1 <anuradha.krishnanov7@gmail.com>
2019-08-29 14:15:24 +0530

    List of policies commited by Anuradha

---
+++ src/main/java/com/hcl/insurance/dto/PolicyDetailsDto.java
@@ -0,0 +1,23 @@
+package com.hcl.insurance.dto;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PolicyDetailsDto implements Serializable{
+	
+	private static final long serialVersionUID = -6566203865949694322L;
+	private Integer policyId;
+	private String policyName;
+	private Integer policyMinAge;
+	private Integer policyMaxAge;
+	private Integer policyMaturityAge;
+	private Integer policyTerm;
+	private Double policyPrice;
+	private Double policyOnlinePrice;
+	private Double policyAssuredSum;
+
+}
